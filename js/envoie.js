3b6ac0a9abbc74ee370a8ae9df30ab12d5f108ad
Meli56 <bodiou.amelie@gmail.com>
2022-04-05 16:01:59 +0200

    recup donnees

--- js/envoie.js
+++ js/envoie.js
@@ -3,11 +3,16 @@ $("#myForm").submit(function(e){
     var form_url = $(this).attr("action"); //récupérer l'URL du formulaire
     var form_method = $(this).attr("method"); //récupérer la méthode GET/POST du formulaire
     var form_data = $(this).serialize(); //Encoder les éléments du formulaire pour la soumission
-    
+    var id = $('id').val();
+    var date = $('date').val();
+
+
     $.ajax({
       url : form_url,
       type: form_method,
-      data : form_data
+      data : form_data,
+      id : id,
+      date : date
     }).done(function(response){ 
       $("#res").html(response);
     });
